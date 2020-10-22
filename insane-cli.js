fc05a74ed6e5b4d1e86a5cf05353eb7b1f52c168
Huy Nguyen <mrvietnamese.ca@gmail.com>
2020-10-22 14:07:33 -0400

    Remove semi-colons in the code

--- insane-cli.js
+++ insane-cli.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 // new branch sample
-const fs = require('fs');
-const readline = require('readline');
+const fs = require('fs')
+const readline = require('readline')
 const colors = require('colors')
-const request = require('request');
-const { url } = require('inspector');
+const request = require('request')
+const { url } = require('inspector')
 
 
 // patterns & splitter
 const PT1 = /\bhttps?::\/\/\S+/gi
 const PT2 = /\bhttps?:\/\/\S+/gi
 // wayback api
-const waybackApi = "http://archive.org/wayback/available?";
+const waybackApi = "http://archive.org/wayback/available?"
 
 
 // let filename = process.argv[2]
@@ -21,17 +21,17 @@ let argv4 = process.argv[3]
 
 const readIgnoreFile = async () => {
     let ignoreURL = []
-    const fileStream = fs.createReadStream(process.argv[4]);
+    const fileStream = fs.createReadStream(process.argv[4])
     const rl = readline.createInterface({
         input: fileStream,
-    });
+    })
 
     for await (const line of rl) {
         if (!line.startsWith('#')) {
             ignoreURL.push(line)
         }
     }
-    return ignoreURL;
+    return ignoreURL
 }
 
 if (argv3 != null) {
@@ -39,43 +39,43 @@ if (argv3 != null) {
         if (argv3 === '-url' || argv3 === '/url') {
             request(argv4, function (error, response, body) {
                 if (response.statusCode === 200) { // only process url with statusCode 200
-                    let tempURL = getURLs(body);
-                    printURLStatus(tempURL);
+                    let tempURL = getURLs(body)
+                    printURLStatus(tempURL)
                 } else {
-                    console.log('Bad link'.red);
+                    console.log('Bad link'.red)
                 }
-            });
+            })
 
         } else if (argv3 === '-w' || argv3 === '/w') {
             // This is for searching webpage in wayback machine
             // usage syntax: insane-cli -w url yyyy-mm-dd-hh-ss
             // The timestamp is optional, and if not provided, it will
             // retrieve the lates archived version of the page
-            let timeStamp = "";
+            let timeStamp = ""
             if (process.argv[4]) {
-                timeStamp = process.argv[4];
+                timeStamp = process.argv[4]
             }
-            const url = `${waybackApi}url=${argv4}&timestamp=${timeStamp}`;
+            const url = `${waybackApi}url=${argv4}&timestamp=${timeStamp}`
             request(url, function (error, response, body) {
-                const { archived_snapshots } = JSON.parse(body);
+                const { archived_snapshots } = JSON.parse(body)
                 if (Object.keys(archived_snapshots).length === 0 && archived_snapshots.constructor === Object) {
-                    console.log("No archived record found");
+                    console.log("No archived record found")
                 }
                 else {
-                    const year = archived_snapshots.closest.timestamp.substring(0, 4);
-                    const month = archived_snapshots.closest.timestamp.substring(4, 6);
-                    const day = archived_snapshots.closest.timestamp.substring(6, 8);
-                    const hour = archived_snapshots.closest.timestamp.substring(8, 10);
-                    const min = archived_snapshots.closest.timestamp.substring(10, 12);
-                    const sec = archived_snapshots.closest.timestamp.substring(12, 14);
-                    console.log(`Archived URL: ${archived_snapshots.closest.url}`);
-                    console.log(`Archived Time: ${year}-${month}-${day} ${hour}-${min}-${sec}`);
+                    const year = archived_snapshots.closest.timestamp.substring(0, 4)
+                    const month = archived_snapshots.closest.timestamp.substring(4, 6)
+                    const day = archived_snapshots.closest.timestamp.substring(6, 8)
+                    const hour = archived_snapshots.closest.timestamp.substring(8, 10)
+                    const min = archived_snapshots.closest.timestamp.substring(10, 12)
+                    const sec = archived_snapshots.closest.timestamp.substring(12, 14)
+                    console.log(`Archived URL: ${archived_snapshots.closest.url}`)
+                    console.log(`Archived Time: ${year}-${month}-${day} ${hour}-${min}-${sec}`)
                 }
-            });
+            })
         } else if (argv3 === '-j' || argv3 === '--json' || argv3 === '/j') {
             // do something to print JSON from here
 
-            let filename = argv4;
+            let filename = argv4
 
             try {
                 if (fs.existsSync(filename)) { // check if file exist
@@ -88,20 +88,20 @@ if (argv3 != null) {
 
                             })
                             .catch(function (error) {
-                                console.log(error);
+                                console.log(error)
                             })
                     })()
                 } else {
-                    console.log('File not found!');
+                    console.log('File not found!')
                 }
             } catch (err) {
-                console.error(err);
+                console.error(err)
             }
 
         } else if (argv3 === '--all' || argv3 === '--good' || argv3 === '--bad') {
             ///////////////
 
-            let filename = argv4;
+            let filename = argv4
 
             try {
                 if (fs.existsSync(filename)) { // check if file exist
@@ -114,14 +114,14 @@ if (argv3 != null) {
 
                             })
                             .catch(function (error) {
-                                console.log(error);
+                                console.log(error)
                             })
                     })()
                 } else {
-                    console.log('File not found!');
+                    console.log('File not found!')
                 }
             } catch (err) {
-                console.error(err);
+                console.error(err)
             }
 
 
@@ -129,7 +129,7 @@ if (argv3 != null) {
 
         } else if (argv4 === '-i' || argv4 === '--ignore') {
             readIgnoreFile().then(lists => {
-                let filename = argv3;
+                let filename = argv3
 
                 try {
                     if (fs.existsSync(filename)) { // check if file exist
@@ -138,10 +138,10 @@ if (argv3 != null) {
                                 .then(function (data) {
                                     let finalURLs = getURLs(data)
                                     finalURLs = finalURLs.filter(url => {
-                                        let flag = true;
+                                        let flag = true
                                         for (let i = 0; i < lists.length; i++) {
                                             if (url.includes(lists[i])){
-                                                flag = false;
+                                                flag = false
                                             } 
                                         }
                                         return flag
@@ -150,24 +150,24 @@ if (argv3 != null) {
 
                                 })
                                 .catch(function (error) {
-                                    console.log(error);
+                                    console.log(error)
                                 })
                         })()
                     } else {
-                        console.log('File not found!');
+                        console.log('File not found!')
                     }
                 } catch (err) {
-                    console.error(err);
+                    console.error(err)
                 }
             })
         } else {
-            console.log('Command not found!');
+            console.log('Command not found!')
         }
     } else {
         if (argv3 === '--version' || argv3 === '/v') {
             console.log('INSANE-CLI VERSION 0.69'.green)
         } else { // for local file process
-            let filename = argv3;
+            let filename = argv3
 
             try {
                 if (fs.existsSync(filename)) { // check if file exist
@@ -180,14 +180,14 @@ if (argv3 != null) {
 
                             })
                             .catch(function (error) {
-                                console.log(error);
+                                console.log(error)
                             })
                     })()
                 } else {
-                    console.log('File not found!');
+                    console.log('File not found!')
                 }
             } catch (err) {
-                console.error(err);
+                console.error(err)
             }
         }
     }
@@ -224,7 +224,7 @@ function printURLStatus(urls) {
             request(urls[i], { method: 'HEAD', timeout: 1800 }, function (error, response, body) {
 
                 //console.error('error:', error);
-                let status = response && response.statusCode;
+                let status = response && response.statusCode
                 if (status !== null) {
                     if (status === 200) {
                         console.log('[' + status + ']GOOD - ' + urls[i].green)
@@ -236,7 +236,7 @@ function printURLStatus(urls) {
                         console.log('UNKNOWN - ' + urls[i].grey)
                     }
                 }
-            });
+            })
         } catch (error) {
             console.error('WTF - ' + urls[i].yellow)
         }
@@ -250,7 +250,7 @@ function printURLStatusByFlag(urls, flag) {
             request(urls[i], { method: 'HEAD', timeout: 1800 }, function (error, response, body) {
 
                 //console.error('error:', error);
-                let status = response && response.statusCode;
+                let status = response && response.statusCode
                 if (status !== null) {
                     // if (status === 200) {
                     //     console.log('[' + status + ']GOOD - ' + urls[i].green)
@@ -281,7 +281,7 @@ function printURLStatusByFlag(urls, flag) {
                         }
                     }
                 }
-            });
+            })
         } catch (error) {
             console.error('WTF - ' + urls[i].yellow)
         }
@@ -290,16 +290,16 @@ function printURLStatusByFlag(urls, flag) {
 
 function printURLStatusInJSON(urls) {
 
-    var res = [];
+    var res = []
 
-    let str = "";
+    let str = ""
     for (let i = urls.length; i--;) {
         getObj(urls[i]).then((data) => {
-            console.log(JSON.parse(JSON.stringify(data)));
+            console.log(JSON.parse(JSON.stringify(data)))
         })
     }
 
-    console.log(str);
+    console.log(str)
 }
 
 function getObj(input) {
@@ -308,19 +308,19 @@ function getObj(input) {
             request(input, { method: 'HEAD', timeout: 1800 }, function (error, response, body) {
 
                 //console.error('error:', error);
-                let status = response && response.statusCode;
+                let status = response && response.statusCode
                 if (status !== null) {
                     var obj = {
                         url: input,
                         statuscode: status
-                    };
-                    resolve(obj);
+                    }
+                    resolve(obj)
                 }
-            });
+            })
         } catch (error) {
             //console.error('WTF - ' + urls[i].yellow)
         }
-    });
+    })
 }
 
 
